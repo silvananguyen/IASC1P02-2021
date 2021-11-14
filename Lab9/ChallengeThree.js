1edcde7d2a7ce2f1ac0316f1f40de11deff40ab8
sn17dm <90402886+sn17dm@users.noreply.github.com>
2021-11-14 16:29:58 -0500

    Delete ChallengeThree.js

--- Lab9/ChallengeThree.js
+++
@@ -1,9 +0,0 @@
-function newValue(){
-  var firstInput = Number(document.getElementById("firstInput").value);
-  console.log(firstInput);
-  var secondInput = Number(document.getElementById("secondInput").value);
-  console.log(secondInput);
-  var addition = firstInput + secondInput;
-  console.log(addition);
-  document.getElementById("outputValue").innerHTML = addition;
-}
