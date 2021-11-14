add534fd1647e4ca4bfc4749cf0159e1f5739f30
silvananguyen <sn17dm@brocku.ca>
2021-11-14 16:31:10 -0500

    Lab 9

--- Lab9/ChallengeThree.js
+++ Lab9/ChallengeThree.js
@@ -1,9 +1,6 @@
-function newValue(){
-  var firstInput = Number(document.getElementById("firstInput").value);
-  console.log(firstInput);
-  var secondInput = Number(document.getElementById("secondInput").value);
-  console.log(secondInput);
-  var addition = firstInput + secondInput;
-  console.log(addition);
-  document.getElementById("outputValue").innerHTML = addition;
+function addOutput(){
+  var inputOne = document.getElementById("firstInput");
+  var inputTwo = document.getElementById("secondInput");
+  var finalValue = Number(firstInput.value) + Number(secondInput.value);
+document.getElementById("addOutput").innerHTML= finalValue;
 }
