b367119374a23876baf900495596975bfed56bd8
silvananguyen <sn17dm@brocku.ca>
2021-11-20 20:30:31 -0500

    Updated Lab 10 Final Challenge

--- Lab10/FinalChallenge.js
+++ Lab10/FinalChallenge.js
@@ -1,21 +1,17 @@
 var elementOne = document.getElementById("firstValue");
 var elementTwo = document.getElementById("secondValue");
-
 function sum(){
   var math = Number(elementOne.value) + Number(elementTwo.value);
   document.getElementById("finalAnswer").innerHTML = math;
 }
-
-function difference(){
+function substract(){
   var math = Number(elementOne.value) - Number(elementTwo.value);
   document.getElementById("finalAnswer").innerHTML = math;
 }
-
 function product(){
   var math = Number(elementOne.value) * Number(elementTwo.value);
   document.getElementById("finalAnswer").innerHTML = math;
 }
-
 function quotient(){
   var math = Number(elementOne.value) / Number(elementTwo.value);
   document.getElementById("finalAnswer").innerHTML = math;
