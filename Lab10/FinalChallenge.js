15eacd60c4519d19f2f38ab67163678bd4339ca7
silvananguyen <sn17dm@brocku.ca>
2021-11-21 16:19:16 -0500

    Updated Final Challenge JS

--- Lab10/FinalChallenge.js
+++ Lab10/FinalChallenge.js
@@ -1,18 +1,19 @@
-var elementOne = document.getElementById("firstValue");
-var elementTwo = document.getElementById("secondValue");
+var firstNumber = document.getElementById("firstValue");
+var secondNumber = document.getElementById("secondValue");
+
 function sum(){
-  var math = Number(elementOne.value) + Number(elementTwo.value);
+  var math = Number(firstNumber.value) + Number(secondNumber.value);
   document.getElementById("finalAnswer").innerHTML = math;
 }
 function substract(){
-  var math = Number(elementOne.value) - Number(elementTwo.value);
+  var math = Number(firstNumber.value) - Number(secondNumber.value);
   document.getElementById("finalAnswer").innerHTML = math;
 }
 function product(){
-  var math = Number(elementOne.value) * Number(elementTwo.value);
+  var math = Number(firstNumber.value) * Number(secondNumber.value);
   document.getElementById("finalAnswer").innerHTML = math;
 }
 function quotient(){
-  var math = Number(elementOne.value) / Number(elementTwo.value);
+  var math = Number(firstNumber.value) / Number(secondNumber.value);
   document.getElementById("finalAnswer").innerHTML = math;
 }
